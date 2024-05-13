8c46a2c21e094e11ed34b4827a51fd73c26bd70e
Enkhbold Ganbold <enkhbold470@gmail.com>
2024-05-13 10:31:34 -0700

    proof of concept

---
+++ src/app/components/landing.tsx
@@ -0,0 +1,7 @@
+export default function Landing() {
+  return (
+    <div className="bg-gradient-radial from-white to-blue-500 h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-black">Join De Anza Hacks</h1>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import Navbar from "./components/navbar";
+import Landing from "./components/landing";
 export default function Page() {
   return (
     <>
       <Navbar />
-      <div className="bg-gradient-radial from-white to-blue-500 h-screen flex items-center justify-center">
-        <h1 className="text-5xl font-bold text-black">Join De Anza Hacks</h1>
-      </div>
+      <Landing />
     </>
   );
 }
