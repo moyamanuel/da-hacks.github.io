62bc482e20d43a978a5ab0abfc3c1317310d380c
Enkhbold470 <enkhbold470@gmail.com>
2024-05-18 05:04:50 -0700

    some bug added haha, jk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
+import Head from "next/head";
+import Script from "next/script";
+
 import { Inter, Open_Sans } from "next/font/google";
 import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
 const openSans = Open_Sans({
   subsets: ["latin"],
@@ -10,9 +12,7 @@ const openSans = Open_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "De Anza Hacks V2.5 2024",
-  description:
-    "Cupertino’s leading hackathon is back! Join us for our 3rd year with 200+ hackers across the state!",
+  viewport: "width=device-width, initial-scale=1.0",
 };
 
 export default function RootLayout({
@@ -22,6 +22,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>De Anza Hacks V2.5 2024</title>
+        <meta
+          name="description"
+          content="Cupertino’s leading hackathon is back! Join us for our 3rd year with 200+ hackers across the state!"
+          key="desc"
+        />
+        <meta
+          property="og:title"
+          content="De Anza Hacks Cupertino’s leading hackathon is back!"
+        />
+        <meta
+          property="og:description"
+          content="Join us for our De Anza Hacks with 200+ hackers across the state!"
+        />
+        <meta
+          property="og:image"
+          content="https://da-hacks.github.io/dadev-bg.jpg"
+        />
+      </Head>
       <body className={` ${openSans.className} bg-slate-950`}>{children}</body>
     </html>
   );
