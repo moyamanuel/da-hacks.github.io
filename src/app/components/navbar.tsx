59d4d8f6e30d5ce8f797c50019d2862a70f5020e
mljoshi <mljoshiro@gmail.com>
2024-05-20 18:44:24 -0700

    fix hamburger not closing on mobile

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -138,6 +138,7 @@ export default function Navbar() {
                     key={item.name}
                     href={item.href}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:bg-slate-400"
+                    onClick={() => setMobileMenuOpen(false)}
                   >
                     {item.name}
                   </a>
