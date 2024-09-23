e6c5ed85f91d02fdc869ffb4f295ace99ddf72a5
enkhbold470 <enkhbold470@gmail.com>
2024-09-23 01:30:11 -0700

    chore: Add lazy loading for images in the FAQ component

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -1,40 +1,57 @@
 "use client";
-import { motion } from "framer-motion"; // Import motion from framer-motion
+import { motion, useInView } from "framer-motion"; // Import useInView from framer-motion
 import { faq, FAQProps } from "@/add_info/faq";
+import React, { useRef } from "react";
 
 export default function Faq() {
+  const faqRef = useRef(null);
+  const isInView = useInView(faqRef, { once: true }); // Ensure this is set up correctly
+
   return (
-    <section // Change section to motion.section
+    <motion.section
       id="faqs"
-      className=" text-gray-100 py-32 min-h-screen bg-gradient-to-b from-slate-950 via-green-800 to-slate-950"
+      className=" text-gray-100 py-32 min-h-screen bg-gradient-to-b from-slate-950 via-green-800 to-slate-950 overflow-hidden"
+      ref={faqRef} // Attach ref to section for visibility tracking
+      initial={{ opacity: 0, x: -50 }}
+      animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: -50 }} // Animate based on visibility
+      transition={{ duration: 0.5 }}
     >
       <div className="container flex flex-col justify-center p-4 mx-auto md:p-8">
         <h2 className="mb-12 text-4xl font-bold text-center">
           Frequently Asked Questions
         </h2>
-        <div className="flex flex-col divide-y sm:px-8 lg:px-12 xl:px-32 divide-gray-200">
+        <div className="flex flex-col divide-y sm:px-8 lg:px-12 xl:px-32 divide-gray-200 overflow-hidden">
           {faq.map((item: FAQProps, index: number) => (
-            <motion.details // Change details to motion.details
+            <motion.details
               key={index}
-              initial={{ opacity: 0, x: -50 }} // Initial position for details
-              animate={{ opacity: 1, x: 0 }} // Animate to original position
-              transition={{ duration: 0.3 }} // Transition duration for details
+              initial={{ opacity: 0, x: 500 }}
+              animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: 500 }} // Animate based on visibility
+              transition={{ duration: 0.7 }}
             >
               <motion.summary
                 className="py-2 outline-none cursor-pointer focus:underline"
-                initial={{ opacity: 0, x: 500 }} // Initial position for details
-                animate={{ opacity: 1, x: 0 }} // Animate to original position
-                transition={{ duration: 0.5 }} // Transition duration for details
+                initial={{ opacity: 0, x: 500 }}
+                animate={
+                  isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: 500 }
+                } // Animate based on visibility
+                transition={{ duration: 0.7 }}
               >
                 {item.question}
               </motion.summary>
-              <div className="px-4 pb-4">
+              <motion.div
+                className="px-4 pb-4"
+                initial={{ opacity: 0, x: 500 }}
+                animate={
+                  isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: 500 }
+                } // Animate based on visibility
+                transition={{ duration: 0.9 }}
+              >
                 <p>{item.answer}</p>
-              </div>
+              </motion.div>
             </motion.details>
           ))}
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 }
